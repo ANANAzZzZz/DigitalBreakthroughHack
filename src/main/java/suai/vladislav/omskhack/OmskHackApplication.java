9606ea5574b29b82fb291a36df5a8b95681989a4
ANANAzZzZz <vladscooltrol@gmail.com>
2024-09-06 20:03:22 +0300

    initial commit

---
+++ src/main/java/suai/vladislav/omskhack/OmskHackApplication.java
@@ -0,0 +1,12 @@
+package suai.vladislav.omskhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmskHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmskHackApplication.class, args);
+    }
+}
---
+++ src/test/java/suai/vladislav/omskhack/OmskHackApplicationTests.java
@@ -0,0 +1,13 @@
+package suai.vladislav.omskhack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmskHackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
